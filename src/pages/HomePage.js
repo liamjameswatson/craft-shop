f5af38447347286d4c8798f2e88637ed1c317c5e
Liam <liamjwatson@aol.com>
2023-06-16 15:15:27 +0100

    carousel added to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Carousel from "../components/Carousel";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
-      HomePage <h1>The Craft shop</h1>
+      <Carousel />
+      {/* HomePage <h1>The Craft shop</h1> */}
     </div>
   );
 };
