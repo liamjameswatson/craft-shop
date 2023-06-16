7ef5c334383e820a46c86d0cfb3785693db6ad24
Liam <liamjwatson@aol.com>
2023-06-16 17:51:44 +0100

    category section added to homepage

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 export const carouselSlides = [
   {
     id: 1,
-    image: "https://images.pexels.com/photos/3625632/pexels-photo-3625632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image:
+      "https://images.pexels.com/photos/3625632/pexels-photo-3625632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Welcome to Our Art Gallery",
     description: "Journey into a World of Creativity and Nature",
     buttonText: "Discover the Art Collection",
@@ -9,7 +10,8 @@ export const carouselSlides = [
   },
   {
     id: 2,
-    image: "https://images.pexels.com/photos/965984/pexels-photo-965984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image:
+      "https://images.pexels.com/photos/965984/pexels-photo-965984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Unlock the Magic of Handcrafted Jewelry",
     description: "Your Perfect Piece: Handmade Jewelry Collection",
     buttonText: "Explore Unique Pieces",
@@ -17,10 +19,29 @@ export const carouselSlides = [
   },
   {
     id: 3,
-    image: "https://images.pexels.com/photos/9018277/pexels-photo-9018277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image:
+      "https://images.pexels.com/photos/9018277/pexels-photo-9018277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Explore All Pieces",
     description: "Our entire selection",
     buttonText: "Shop Now",
     color: "#dbea86bd",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    image: "https://i.etsystatic.com/22074732/r/il/762dc1/2807751733/il_794xN.2807751733_6j0q.jpg",
+    title: "flower paintings",
+  },
+  {
+    id: 2,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyQAmqJ9PH2Yy2DYEuxh171YvXVHdZTRtAfUBYjKW2Sh_0zFMED7lbKUy6SJNgPhCPZXc&usqp=CAU",
+    title: "animal portraits",
+  },
+  {
+    id: 3,
+    image: "https://www.creativetorbay.com/media/creative-torbay/images/coastal-poppies.jpg",
+    title: "landscapes",
   },
 ];
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Carousel from "../components/Carousel";
+import Categories from "../components/Categories";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
       <Carousel />
+      <Categories />
       {/* HomePage <h1>The Craft shop</h1> */}
     </div>
   );
