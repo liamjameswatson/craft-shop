e69a87c6cfba6eac88c42d590ef5931af520853e
Liam <liamjwatson@aol.com>
2023-06-19 12:36:04 +0100

    product page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <ProductPage />
     </div>
   );
 }
