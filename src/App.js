230ea088f48e6703dd4028df4729fb34caeb5e1d
Liam <liamjwatson@aol.com>
2023-06-19 14:46:52 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 // import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import RegisterPage from "./pages/Register";
+// import ProductPage from "./pages/ProductPage";
+// import RegisterPage from "./pages/Register";
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <div>
       {/* <HomePage /> */}
-      <RegisterPage />
+      <LoginPage />
     </div>
   );
 }
