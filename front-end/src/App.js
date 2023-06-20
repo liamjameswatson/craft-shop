5137835edb0783c015b4892d7a327bdf1d1b0f9b
Liam <liamjwatson@aol.com>
2023-06-20 23:17:35 +0100

    index.js file added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-// import ProductPage from "./pages/ProductPage";
-// import RegisterPage from "./pages/Register";
-// import Basket from "./pages/Basket";
-// import LoginPage from "./pages/LoginPage";
+import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/Register";
+import Basket from "./pages/Basket";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
