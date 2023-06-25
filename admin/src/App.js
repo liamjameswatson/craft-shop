8a5dff99d35550b687dca461ed683abdd7d90cce
Liam <liamjwatson@aol.com>
2023-06-25 14:10:13 +0100

    implemented react-router-dom

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,15 +3,30 @@ import ListAdmin from "./pages/listAdmin/ListAdmin.js";
 import NewAdmin from "./pages/newAdmin/NewAdmin.js";
 import SingleAdmin from "./pages/singleAdmin/SingleAdmin.js";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <HomePageAdmin />
-    <ListAdmin />
-    <SingleAdmin />
-    <NewAdmin />
-
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePageAdmin />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<ListAdmin />} />
+            <Route path=":userId" element={<SingleAdmin />} />
+            <Route path="new" element={<NewAdmin />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<ListAdmin />} />
+            <Route path=":productId" element={<SingleAdmin />} />
+            <Route path="new" element={<NewAdmin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
