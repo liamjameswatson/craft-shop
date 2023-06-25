f724b4757abd07f3db49416e5847a5c0eb10cd92
Liam <liamjwatson@aol.com>
2023-06-25 17:36:51 +0100

    navbar added

--- admin/src/components/navBar/NavBar.js
+++ admin/src/components/navBar/NavBar.js
@@ -1,10 +1,35 @@
 import "./navBar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon className="icon" />
+        </div>
+        <div className="items">
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
-
+export default NavBar;
--- admin/src/pages/homeAdmin/HomePageAdmin.js
+++ admin/src/pages/homeAdmin/HomePageAdmin.js
@@ -1,12 +1,17 @@
+import NavBar from "../../components/navBar/NavBar";
 import SideBar from "../../components/sidebar/SideBar";
 import "./homePageAdmin.scss";
 
 const HomePageAdmin = () => {
-  return <div className="home">
-    <SideBar />
-    <div className="homeContainer">container</div>
-  </div>;
-
+  return (
+    <div className="home">
+      <SideBar />
+      <div className="homeContainer">
+        <NavBar />
+        HOME CONTAINER
+      </div>
+    </div>
+  );
 };
 
 export default HomePageAdmin;
